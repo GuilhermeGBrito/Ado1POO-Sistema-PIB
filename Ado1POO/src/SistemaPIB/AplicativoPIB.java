185d58ff6b4639a20ac48feb9aa6d95695ca22e7
GuilhermeGBrito <guimaresbrito03@gmail.com>
2023-08-11 16:45:57 -0300

    Finalizando

--- Ado1POO/src/SistemaPIB/AplicativoPIB.java
+++ Ado1POO/src/SistemaPIB/AplicativoPIB.java
@@ -13,6 +13,8 @@ import java.util.Map;
 public class AplicativoPIB {
     private List<Estado> estados;
     private Map<String, Regiao> regioes;
+    
+    
 
     public AplicativoPIB() {
         this.estados = new ArrayList<>();
@@ -20,37 +22,38 @@ public class AplicativoPIB {
     }
 
     public void carregarEstados(String arquivo) throws IOException {
-        BufferedReader reader = new BufferedReader(new FileReader(arquivo));
-        String linha;
-        while ((linha = reader.readLine()) != null) {
-            String[] dados = linha.split(";");
-            if (dados.length == 2) {
-                Estado estado = new Estado(dados[0], Double.parseDouble(dados[1]));
-                estados.add(estado);
+        try (BufferedReader reader = new BufferedReader(new FileReader("C:\\Users\\guivi\\eclipse-workspace\\Ado1POO\\src\\SistemaPIB\\pib.txt"))) {
+            String linha;
+            while ((linha = reader.readLine()) != null) {
+                String[] dados = linha.split(";");
+                if (dados.length == 2) {
+                    Estado estado = new Estado(dados[0], Double.parseDouble(dados[1]));
+                    estados.add(estado);
+                }
             }
         }
-        reader.close();
     }
 
     public void carregarRegioes(String arquivo) throws IOException {
-        BufferedReader reader = new BufferedReader(new FileReader(arquivo));
-        String linha;
-        Regiao regiaoAtual = null;
-        while ((linha = reader.readLine()) != null) {
-            if (!linha.isEmpty()) {
-                if (linha.contains(";")) {
-                    String[] dados = linha.split(";");
-                    regiaoAtual = new Regiao(dados[0]);
-                    regioes.put(dados[0], regiaoAtual);
-                } else if (regiaoAtual != null) {
-                    Estado estado = estados.stream().filter(e -> e.nome.equals(linha)).findFirst().orElse(null);
-                    if (estado != null) {
-                        regiaoAtual.adicionarEstado(estado);
+        try (BufferedReader reader = new BufferedReader(new FileReader("C:\\Users\\guivi\\eclipse-workspace\\Ado1POO\\src\\SistemaPIB\\regioes.txt"))) {
+            String linha;
+            Regiao regiaoAtual = null;
+            while ((linha = reader.readLine()) != null) {
+                if (!linha.isEmpty()) {
+                    if (linha.contains(";")) {
+                        String[] dados = linha.split(";");
+                        regiaoAtual = new Regiao(dados[0]);
+                        regioes.put(dados[0], regiaoAtual);
+                    } else if (regiaoAtual != null) {
+                        final String tempLinha = linha;
+                        Estado estado = estados.stream().filter(e -> e.nome.equals(tempLinha)).findFirst().orElse(null);
+                        if (estado != null) {
+                            regiaoAtual.adicionarEstado(estado);
+                        }
                     }
                 }
             }
         }
-        reader.close();
     }
 
     public void imprimirPIBPorEstado() {
@@ -62,11 +65,11 @@ public class AplicativoPIB {
     }
 
     public void gerarArquivoSaida(String arquivo) throws IOException {
-        PrintWriter writer = new PrintWriter(new FileWriter("New.txt"));
-        for (Regiao regiao : regioes.values()) {
-            double totalPIBRegiao = regiao.estados.stream().mapToDouble(e -> e.pib).sum();
-            writer.printf("%s: %.2f%n", regiao.nome, totalPIBRegiao);
+        try (PrintWriter writer = new PrintWriter(new FileWriter("C:\\Users\\guivi\\eclipse-workspace\\Ado1POO\\src\\SistemaPIB\\New.txt"))) {
+            for (Regiao regiao : regioes.values()) {
+                double totalPIBRegiao = regiao.estados.stream().mapToDouble(e -> e.pib).sum();
+                writer.printf("%s: %.2f%n", regiao.nome, totalPIBRegiao);
+            }
         }
-        writer.close();
     }
 }
--- Ado1POO/src/SistemaPIB/PibMain.java
+++ Ado1POO/src/SistemaPIB/PibMain.java
@@ -3,19 +3,19 @@ package SistemaPIB;
 import java.io.IOException;
 
 public class PibMain {
-	
-	  public static void main(String[] args) {
-	        AplicativoPIB app = new AplicativoPIB();
-	        try {
-	            app.carregarEstados("pib.txt");
-	            app.carregarRegioes("regioes.txt");
 
-	            System.out.println("Percentual de PIB por estado:");
-	            app.imprimirPIBPorEstado();
+	public static void main(String[] args) {
+		AplicativoPIB app = new AplicativoPIB();
+		try {
+			app.carregarEstados("C:\\Users\\guivi\\eclipse-workspace\\Ado1POO\\src\\SistemaPIB\\pib.txt");
+			app.carregarRegioes("C:\\Users\\guivi\\eclipse-workspace\\Ado1POO\\src\\SistemaPIB\\regioes.txt");
 
-	            app.gerarArquivoSaida("saida.txt");
-	        } catch (IOException e) {
-	            e.printStackTrace();
-	        }
-	    }
+			System.out.println("Percentual de PIB por estado:");
+			app.imprimirPIBPorEstado();
+
+			app.gerarArquivoSaida("C:\\Users\\guivi\\eclipse-workspace\\Ado1POO\\src\\SistemaPIB\\\\New.txt");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
+}
