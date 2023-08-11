bbb2f87b253632fffb76501b76f750d986175de9
GuilhermeGBrito <guimaresbrito03@gmail.com>
2023-08-11 11:35:11 -0300

    att

--- Ado1POO/src/SistemaPIB/Estado.java
+++ Ado1POO/src/SistemaPIB/Estado.java
@@ -1,14 +1,12 @@
 package SistemaPIB;
 
 public class Estado {
-	
-	String nome;
-	double pib;
-	
-	Estado(String nome, double pib){
-		this.nome = nome;
-		this.pib = pib;
-		
-	}
 
+    String nome;
+    double pib;
+
+    public Estado(String nome, double pib) {
+        this.nome = nome;
+        this.pib = pib;
+    }
 }
--- Ado1POO/src/SistemaPIB/Regiao.java
+++ Ado1POO/src/SistemaPIB/Regiao.java
@@ -4,20 +4,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Regiao {
-	
-	String nome;
-	List<Estado> estados;
-	
-	public Regiao(String nome) {
-		this.nome = nome;
-		this.estados =  new ArrayList<>();
-	}
-	
-	void adicionarEstado(Estado estado){
-		estados.add(estado);
-	}
-	
-	
-	
 
+    String nome;
+    List<Estado> estados;
+
+    public Regiao(String nome) {
+        this.nome = nome;
+        this.estados = new ArrayList<>();
+    }
+
+    public void adicionarEstado(Estado estado) {
+        estados.add(estado);
+    }
 }
